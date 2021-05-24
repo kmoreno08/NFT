a9c3fb613756d0abaa9fdee47d0e1852cb38eb4a
Kevin Moreno <moren162@mail.chapman.edu>
2021-05-23 18:15:02 -0700

    first commit

---
+++ data/data.js
@@ -0,0 +1,234 @@
+// [{
+//   "artist": "christopher lizarraga",
+//   "painting": "even shadows",
+//   "location": "(../img/artist/chris_lizarraga/even_shadows.png)"
+// },
+// {
+//   "artist": "christopher lizarraga",
+//   "painting": "kinetic",
+//   "location": "(../img/artist/chris_lizarraga/kinetic.jpg)"
+// },
+// {
+//   "artist": "christopher lizarraga",
+//   "painting": "the new normal",
+//   "location": "(../img/artist/chris_lizarraga/the_new_normal.jpg)"
+// },
+// {
+//   "artist": "christopher lizarraga",
+//   "painting": "trapped",
+//   "location": "(../img/artist/chris_lizarraga/trapped.jpg)"
+// },
+// {
+//   "artist": "dean and james",
+//   "painting": "a window of opportunity",
+//   "location": "(../img/artist/dean_and_james/a_window_of_opportunity.jpg)"
+// },
+// {
+//   "artist": "dean and james",
+//   "painting": "facing reality",
+//   "location": "(../img/artist/dean_and_james/facing_reality.jpg)"
+// },
+// {
+//   "artist": "dean and james",
+//   "painting": "the storm",
+//   "location": "(../img/artist/dean_and_james/the_storm.jpg)"
+// },
+// {
+//   "artist": "florian eyemann",
+//   "painting": "flowers",
+//   "location": "(../img/artist/florian_eyemann/flowers.jpg)"
+// },
+// {
+//   "artist": "florian eyemann",
+//   "painting": "portait",
+//   "location": "(../img/artist/florian_eyemann/portait.jpg)"
+// },
+// {
+//   "artist": "florian eyemann",
+//   "painting": "skulls",
+//   "location": "(../img/artist/florian_eyemann/skulls.jpg)"
+// },
+// {
+//   "artist": "jason sims",
+//   "painting": "3ft rise",
+//   "location": "(../img/artist/jason_sims/3ft_rise.jpg)"
+// },
+// {
+//   "artist": "jason sims",
+//   "painting": "4ft stack",
+//   "location": "(../img/artist/jason_sims/4ft_stack.jpg)"
+// },
+// {
+//   "artist": "jason sims",
+//   "painting": "dual six",
+//   "location": "(../img/artist/jason_sims/dual_vi.jpg)"
+// },
+// {
+//   "artist": "jason sims",
+//   "painting": "light break nexus",
+//   "location": "(../img/artist/jason_sims/light_break--nexus.jpg)"
+// },
+// {
+//   "artist": "jason sims",
+//   "painting": "on the horizon two",
+//   "location": "(../img/artist/jason_sims/on_the_horizon_II.jpg)"
+// },
+// {
+//   "artist": "jason sims",
+//   "painting": "on the horizon arch",
+//   "location": "(../img/artist/jason_sims/on_the_horizon--arch.jpg)"
+// },
+
+// {
+//   "artist": "joseph klibansky",
+//   "painting": "self portrait of a dreamer",
+//   "location": "(../img/artist/joseph_klibansky/self_portrait_of_a_dreamer.jpg)"
+// },
+// {
+//   "artist": "joseph klibansky",
+//   "painting": "take me to paradise",
+//   "location": "(../img/artist/joseph_klibansky/take_me_to_paradise.jpg)"
+// },
+// {
+//   "artist": "joseph klibansky",
+//   "painting": "the thinker",
+//   "location": "(../img/artist/joseph_klibansky/the_thinker.jpg)"
+// },
+// {
+//   "artist": "katya zvereva",
+//   "painting": "california",
+//   "location": "(../img/artist/katya_zvereva/california.jpg)"
+// },
+// {
+//   "artist": "katya zvereva",
+//   "painting": "dreamer",
+//   "location": "(../img/artist/katya_zvereva/dreamer.jpg)"
+// },
+// {
+//   "artist": "katya zvereva",
+//   "painting": "seeker",
+//   "location": "(../img/artist/katya_zvereva/seeker.jpg)"
+// },
+// {
+//   "artist": "mary ronayne",
+//   "painting": "bennie doyle",
+//   "location": "(../img/artist/mary_ronayne/bennie_doyle.jpg)"
+// },
+// {
+//   "artist": "mary ronayne",
+//   "painting": "liverpool wedding",
+//   "location": "(../img/artist/mary_ronayne/liverpool_wedding.jpg)"
+// },
+// {
+//   "artist": "mary ronayne",
+//   "painting": "the darlington",
+//   "location": "(../img/artist/mary_ronayne/the_darlington.jpg)"
+// },
+// {
+//   "artist": "tian yonghua",
+//   "painting": "mascot",
+//   "location": "(../img/artist/tian_yonghua/mascot.jpg)"
+// },
+// {
+//   "artist": "tian yonghua",
+//   "painting": "touch",
+//   "location": "(../img/artist/tian_yonghua/touch.jpg)"
+// },
+// {
+//   "artist": "tian yonghua",
+//   "painting": "virtual mirror",
+//   "location": "(../img/artist/tian_yonghua/virtual_mirror.jpg)"
+// },
+// {
+//   "artist": "tian yonghua",
+//   "painting": "wave 2",
+//   "location": "(../img/artist/tian_yonghua/wave_2.jpg)"
+// }
+
+// ]
+
+
+
+// // data
+// let data = {
+//   christoper_lizarraga: [
+//     {art: 'even shadows', location: '(../img/artist/chris_lizarraga/even_shadows.png)'},
+//     {art: 'kinetic',
+//     location: "(../img/artist/chris_lizarraga/kinetic.jpg)"},
+//    {art: "the new normal",
+//    location: "(../img/artist/chris_lizarraga/the_new_normal.jpg)"},
+//    {art: "trapped",
+//    location: "(../img/artist/chris_lizarraga/trapped.jpg)"},
+//   ],
+
+//   dean_and_james: [
+//     {art: "a window of opportunity",
+//     location: "(../img/artist/dean_and_james/a_window_of_opportunity.jpg)"},
+//     {art: "facing reality",
+//     location: "(../img/artist/dean_and_james/facing_reality.jpg)"},
+//     {art: "the storm",
+//     location: "(../img/artist/dean_and_james/the_storm.jpg)"}
+//   ],
+   
+//   florian_eyemann: [
+//     {art: "flowers",
+//     location: "(../img/artist/florian_eyemann/flowers.jpg)"},
+//     {art: "portait",
+//     location: "(../img/artist/florian_eyemann/portait.jpg)"},
+//     {art: "skulls",
+//     location: "(../img/artist/florian_eyemann/skulls.jpg)"}
+//   ],
+
+//   jason_sims: [
+//     {art: "3ft rise",
+//     location: "(../img/artist/jason_sims/3ft_rise.jpg)"},
+//     {art: "4ft stack",
+//     location: "(../img/artist/jason_sims/4ft_stack.jpg)"},
+//     {art: "dual six",
+//     location: "(../img/artist/jason_sims/dual_vi.jpg)"},
+//     {art: "light break nexus",
+//     location: "(../img/artist/jason_sims/light_break--nexus.jpg)"},
+//     {art: "on the horizon two",
+//     location: "(../img/artist/jason_sims/on_the_horizon_II.jpg)"},
+//     {art: "on the horizon arch",
+//     location: "(../img/artist/jason_sims/on_the_horizon--arch.jpg)"}
+//   ],
+
+//   joseph_klibansky: [
+//     {art: "self portrait of a dreamer",
+//     location: "(../img/artist/joseph_klibansky/self_portrait_of_a_dreamer.jpg)"},
+//     {art: "take me to paradise",
+//     location: "(../img/artist/joseph_klibansky/take_me_to_paradise.jpg)"},
+//     {art: "the thinker",
+//     location: "(../img/artist/joseph_klibansky/the_thinker.jpg)"}
+//   ],
+
+//   katya_zvereva: [
+//     {art: "california",
+//     location: "(../img/artist/katya_zvereva/california.jpg)"},
+//     {art: "dreamer",
+//     location: "(../img/artist/katya_zvereva/dreamer.jpg)"},
+//     {art: "seeker",
+//     location: "(../img/artist/katya_zvereva/seeker.jpg)"}
+//   ],
+
+//   mary_ronayne: [
+//     {art: "bennie doyle",
+//     location: "(../img/artist/mary_ronayne/bennie_doyle.jpg)"},
+//     {art: "liverpool wedding",
+//     location: "(../img/artist/mary_ronayne/liverpool_wedding.jpg)"},
+//     {art: "the darlington",
+//     location: "(../img/artist/mary_ronayne/the_darlington.jpg)"},
+//   ],
+
+//   tian_yonghua: [
+//     {art: "mascot",
+//     location: "(../img/artist/tian_yonghua/mascot.jpg)"},
+//     {art: "touch",
+//     location: "(../img/artist/tian_yonghua/touch.jpg)"},
+//     {art: "virtual mirror",
+//     location: "(../img/artist/tian_yonghua/virtual_mirror.jpg)"},
+//     {art: "wave 2",
+//     location: "(../img/artist/tian_yonghua/wave_2.jpg)"}
+//   ]
+// };
---
+++ js/signup.js
@@ -0,0 +1,6 @@
+// Toggle between sign-up and sign-in forms with picture in background
+document.querySelector('.img-btn').addEventListener('click', function()
+	{
+		document.querySelector('.form__wrapper').classList.toggle('s-signup')
+	}
+);
