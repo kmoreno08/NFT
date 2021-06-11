bdf14a8df982136ad1f68e2a361dff427c2d4fee
Kevin Moreno <moren162@mail.chapman.edu>
2021-06-10 19:10:38 -0700

    SearchBar dynamic/classes updated

--- js/cart.js
+++ js/cart.js
@@ -9,11 +9,6 @@ const cartTotal = document.querySelector(".cart__content");
 const cartContent = document.querySelector(".cart__content")
 const productsDOM = document.querySelector(".products-center");
 const sellersDOM = document.querySelector("#seller");
-// gallery variables
-// Select Each gallery card
-const galleryCard = document.getElementsByClassName("gallery-card");
-// Select Parent of gallery Card
-const galleryWrapperCard = document.querySelector(".gallery__wrapper__card");
 
 
 
@@ -43,6 +38,13 @@ class Art {
 
 }
 
+
+
+// gallery variables
+// Select Each gallery card
+const galleryCard = document.getElementsByClassName("gallery-card");
+// Select Parent of gallery Card
+const galleryWrapperCard = document.querySelector(".gallery__wrapper__card");
 
 class UI {
   // displays artwork for a given an artist
@@ -132,11 +134,127 @@ class UI {
 }
 
 
+// Search box on navigation
+const searchBox = document.querySelector(".nav__left__searchBar");
+// Search box on modal
+const modalSearchBox = document.querySelector(".modal__searchBox--text");
+// Modal that displays on entire page after user clocks on search box on navigation
+const modal = document.querySelector("#modal");
+// User input match will display in here
+const matchList = document.getElementById("modal__content--list");
+
+// Clear button on modalSearchBox
+const clearBtn = document.querySelector(".modal__wrapper__searchBox--clearButton");
+//close button on modalSearchBox
+const closeBtn = document.querySelector(".modal__wrapper__searchBox--closeButton");
+
+
+
+
+
+
+class SearchBar {
+  
+  // Function to clear text box
+ clearTextBox() {
+  modalSearchBox.value = "";
+  clearSearchBar();
+}
+// Function to open modal
+ openModal() {
+  modal.style.display = "block";
+}
+// Function to close modal
+ closeModal() {
+  modal.style.display = "none";
+  clearTextBox();
+}
+
+// match user inputs in search bar and filter
+ matchTextToObject(searchText, artistsObject){
+  // clear search bar
+   this.clearSearchBar();
+  // Get each artist in object
+  artistsObject.forEach(artist => {
+    // All artwork for current artist
+    let artistGallery = artist.art;
+    // save artist name
+    let artistName = artist.name;
+  
+    // Loop through each artWork for current artist
+    artistGallery.forEach(individualArt => {
+      // save variable of current art name
+      let artName = individualArt.name;
+      
+      /* Regex for searchText
+      g = match all instances of the pattern in a string, not just one,
+      i = case-insensitive
+      */
+      const regex = new RegExp(`${searchText}`, "gi");
+      // Check if match between art name and user input
+      const match = artName.match(regex);
+      // if no match, then go to next artwork
+      if(match === null){
+      // if there is a match, then dispay in search bar
+      } else {
+        let imgLocation = individualArt.imgLocation;
+          let price = individualArt.price;
+          // let searchCardInnerHTML = "";
+       this.displaySearchCard(artistName, artName, imgLocation, price);
+      }
+    })
+  })
+}
+
+  searchBarUserInput(artistsObject){
+    // Listen for open click
+    searchBox.addEventListener("click", this.openModal);
+    // Listen for close click
+    closeBtn.addEventListener("click", this.closeModal);
+    // Listen for clear button
+    clearBtn.addEventListener("click", this.clearTextBox);
+
+    /* 
+   Listener for when user types in to modalSearchBox
+   Each key input will run function matchTextToObject
+   */
+   modalSearchBox.addEventListener("input", () =>
+  // Pass input value and artistObject
+   this.matchTextToObject(modalSearchBox.value, artistsObject)
+ );
+    }
+
+ displaySearchCard(artistName, artName, imgLocation, price){
+  // add search card to HTML in matchList
+   matchList.innerHTML += `
+  <div class="searchCard" onmousedown="WillCreateFunctionHere(this)">
+  <div class="searchCard-left">
+    <img
+      src="${imgLocation}"
+      alt=""
+    />
+    <h5>${artName}</h5>
+  </div>
+  <div class="searchCard-right">
+      <h5>${artistName}</h5>
+      <h5>${price}</h5>
+  </div>
+</div>`;
+
+ }
+
+ // Clear searches
+ clearSearchBar(){
+  matchList.innerHTML = "";
+ }
+}
+
 // On load
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Has loaded");
   const art = new Art();
   const ui = new UI();
+  const searchBar = new SearchBar();
 
 
   // get all artists data
@@ -144,16 +262,21 @@ document.addEventListener("DOMContentLoaded", () => {
     artistsObject => {
       // Use random number generator to pick random artist
       let randomArtistObject = ui.getRandomArtist(artistsObject);
+
       // display random artists in gallery
       ui.displayArtwork(randomArtistObject);
+
+      // display specific artist selected by user
       ui.getSpecificArtist(artistsObject);
-      // ui.getSpecificArtist(randomArtistObject);
-      
-    })
+
+      // display search card given user input
+      searchBar.searchBarUserInput(artistsObject);
+  
   // ).then(() => {
   //   // ui.getSpecificArtist();
   // })
-})
+  })
+});
 
 
 
